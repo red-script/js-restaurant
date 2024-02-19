7c251ae6c7a7ed92700d0bdf4cfda909c539818e
red-script <ibnwadud7@gmail.com>
2024-02-19 10:04:24 +0300

    fixing issues

--- src/home.js
+++ src/home.js
@@ -8,7 +8,7 @@ export function homePage() {
     const p3 = document.createElement('p');
 
     h1.textContent = "Welcome to Delicious Delights Restaurant";
-    img.src = "/src/img/image.jpg";
+    img.src = "src/img/image.jpg";
     p1.textContent = "At Delicious Delights, we pride ourselves on serving the most delicious and mouth-watering dishes you'll ever taste. Our talented chefs use only the finest and freshest ingredients to create culinary delights that will tantalize your taste buds.";
     p2.textContent = "Whether you're craving savory classics or exploring adventurous flavors, our diverse menu has something for everyone. From appetizers to desserts, each dish is crafted with passion and attention to detail, ensuring an unforgettable dining experience.";
     p3.textContent = "Come dine with us and discover why our loyal customers keep coming back for more. Join us at Delicious Delights Restaurant for an extraordinary culinary journey that will leave you craving for more.";
